a57d19ea446a0aaeddac7c02b4f6222b5d27d7dd
yuzu <s.yuzu441@gmail.com>
2018-03-10 18:14:36 +0900

    Add question 03

--- src/chapter01.rs
+++ src/chapter01.rs
@@ -13,7 +13,7 @@ pub fn q01() -> String {
     return x.chars()
         .enumerate()
         .filter(|&(idx, _c)| idx % 2 == 0)
-        .map(|(_, c)| c )
+        .map(|(_, c)| c)
         .collect::<String>();
 }
 
@@ -34,4 +34,19 @@ pub fn q02() -> String {
 #[test]
 fn q02_test() {
     assert_eq!("パタトクカシーー", q02());
+}
+
+pub fn q03() -> Vec<usize> {
+    let sentence = "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.";
+    let pat: &[_] = &[',', '.'];
+
+    return sentence.split_whitespace()
+        .map(|word| word.trim_right_matches(pat))
+        .map(|w| w.len())
+        .collect::<Vec<usize>>();
+}
+
+#[test]
+fn q03_test() {
+    assert_eq!(vec![3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9], q03());
 }
