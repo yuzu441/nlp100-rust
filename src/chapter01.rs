252f989d41a6ea3b68d9c00f97648e5e4bb1a561
yuzu <s.yuzu441@gmail.com>
2018-03-12 17:31:43 +0900

    Add question 04

--- src/chapter01.rs
+++ src/chapter01.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn q00() -> String {
     let x = "stressed";
     return x.chars().rev().collect::<String>();
@@ -49,4 +51,34 @@ pub fn q03() -> Vec<usize> {
 #[test]
 fn q03_test() {
     assert_eq!(vec![3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5, 8, 9, 7, 9], q03());
+}
+
+pub fn q04<'a>() -> HashMap<&'a str, u32> {
+    let sentence = "Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.";
+    let elements_idx = [1, 5, 6, 7, 8, 9, 15, 16, 19];
+    return sentence.split_whitespace()
+        .enumerate()
+        .map(|(idx, w)| unsafe {
+            let idx = idx + 1; 
+            let c = match elements_idx.contains(&idx) {
+                true => w.slice_unchecked(0, 1),
+                false => w.slice_unchecked(0, 2)
+            };
+            (c, idx as u32)
+        })
+        .collect();
+}
+
+#[test]
+fn q04_test() {
+    let expected: HashMap<&str, u32> = [
+        ("H", 1), ("He", 2), ("Li", 3), ("Be", 4), ("B", 5),
+        ("C", 6), ("N", 7), ("O", 8), ("F", 9), ("Ne", 10),
+        ("Na", 11), ("Mi", 12), ("Al", 13), ("Si", 14), ("P", 15),
+        ("S", 16), ("Cl", 17), ("Ar", 18), ("K", 19), ("Ca", 20)
+    ].iter().cloned().collect();
+    let result = q04();
+    for (key, idx) in expected {
+        assert_eq!(idx, result[key]);
+    }
 }
